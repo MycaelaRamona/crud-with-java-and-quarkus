66f0fde50a6495e1903b633985480a37ab15dd76
mycaelaramona <devramonamartins@gmail.com>
2025-02-13 10:17:54 +0000

    Creation of the Response error

---
+++ src/main/java/io/github/mycaelaramona/quarkussocial/rest/dto/FieldError.java
@@ -0,0 +1,23 @@
+package io.github.mycaelaramona.quarkussocial.rest.dto;
+
+public class Fiielderror {
+
+    private String field;
+    private String message;
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/io/github/mycaelaramona/quarkussocial/rest/dto/ResponseError.java
@@ -0,0 +1,2 @@
+package io.github.mycaelaramona.quarkussocial.rest.dto;public class ResponseError {
+}
